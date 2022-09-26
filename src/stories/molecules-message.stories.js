ecf573948d38db2493c8690deb0be9a5dd0eeaed
unknown <s.yamasaki@4nonome.com>
2022-09-26 14:54:19 +0900

    storybook

---
+++ src/stories/molecules-message.stories.js
@@ -0,0 +1,6 @@
+import Item_Messages from "../components/molecules/message/message.html";
+import "../components/molecules/message/message.scss";
+
+export default{title:"Molecules/Message"};
+
+export const Message = () => Item_Messages;
